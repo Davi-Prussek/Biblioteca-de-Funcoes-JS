125ef684e934823f45a26101e78e97e32263e179
Davi-Prussek <davi.mariano.prussek0916@gmail.com>
2025-08-28 00:33:41 -0300

    Adição de função e correção de arquivos

---
+++ funcoes-de-portugues.js
@@ -0,0 +1,35 @@
+//Função que faz todas as primeiras letras ficarem maiúsculas
+      let letraMaiuscula = (nome) => {
+            let nomeComeco;
+            let restoNome;
+            let nomeCompletoMaiusculo = [];
+            let primeiraLetra = nome.split(" ");
+            for (let i = 0; i < primeiraLetra.length; i++) {
+            nomeComeco = primeiraLetra[i].charAt(0);
+            restoNome = primeiraLetra[i].substring(1);
+            nomeCompletoMaiusculo.push(nomeComeco.toUpperCase() + restoNome);}
+            return nomeCompletoMaiusculo.join(" ");};
+
+//Função que faz a primeira letra de somente UMA PALAVRA ficar maiúscula
+      let letraMaiusculaUnica = (nome) => {
+            let nomeComeco = nome.charAt(0)
+            let restoNome = nome.substring(1);
+            return nome = nomeComeco.toUpperCase() + restoNome;};
+            
+//Função que faz a primeira letra de todas as palavras MENOS AS PREPOSIÇÕES listadas na lista preposicao
+      let letra_Maisc_Prep = (nome) => {
+            let preposicao = ["a", "ante", "após", "até", "com", "contra", "de", "desde", "em",
+            "entre", "para", "perante", "por", "sem", "sob", "sobre", "trás", "da", "de", "do"];
+            let nomeComeco;
+            let restoNome;
+            let nomeCompletoMaiusculo = [];
+            let primeiraLetra = nome.split(" ");
+                  for (let i = 0; i < primeiraLetra.length; i++) {
+                        let palavraMinuscula = primeiraLetra[i].toLowerCase();
+                              if (preposicao.includes(primeiraLetra[i])) {
+                                    nomeCompletoMaiusculo.push(palavraMinuscula);    
+                              }else {
+                                    nomeComeco = primeiraLetra[i].charAt(0);
+                                    restoNome = primeiraLetra[i].substring(1);
+                                    nomeCompletoMaiusculo.push(nomeComeco.toUpperCase() + restoNome)}};
+      return nomeCompletoMaiusculo.join(" ");};
---
+++ funcoes-matematicas.js
@@ -0,0 +1,21 @@
+//Função para somar dois valores
+      let somar = (a, b) => { return a + b};
+
+//Função para subtrair dois valores
+      let subtrair = (a, b) => { return a - b};
+
+//Função para dividir dois valores
+      let dividir = (a, b) => { return a / b};
+
+//Função para multiplicar dois valores
+      let multiplicar = (a, b) => { return a * b};
+
+//Função para potenciação de um número
+      let potencia = (a, b) => { return a ** b};
+
+//Função para descobrir tantos porcento de um valor
+      let calcularValorPorcentagem = (a, b) => { return (a / 100) * b }
+
+//Função para descobrir porcentagem de um valor
+      let calcularPorcentagemDoValor = (a, b) => { return (b / a) * 100 }
+    
